62b140499f45a1cf2ba3225b80c6770ebbe6e968
Rohan Thapliyal <thapliyr@oregonstate.edu>
2024-07-14 13:45:57 -0700

    Adding Js and css to the "about me" section/page

--- main.js
+++ main.js
@@ -1,24 +1,29 @@
 $("#header-home").on('click', function () {
+    $("#about-me").hide();
     $("#nav-bar").css("padding-top", "200px")
     $("#wave-animation").css("padding-top", "50px")
     $(".waves").css("height", "100px")
 });
 $("#header-about").on('click', function () {
+    $("#about-me").show();
     $("#nav-bar").css("padding-top", "0")
     $("#wave-animation").css("padding-top", "0")
     $(".waves").css("height", "40px")
 });
 $("#header-education").on('click', function () {
+    $("#about-me").hide();
     $("#nav-bar").css("padding-top", "0")
     $("#wave-animation").css("padding-top", "0")
     $(".waves").css("height", "40px")
 });
 $("#header-experience").on('click', function () {
+    $("#about-me").hide();
     $("#nav-bar").css("padding-top", "0")
     $("#wave-animation").css("padding-top", "0")
     $(".waves").css("height", "40px")
 });
 $("#header-projects").on('click', function () {
+    $("#about-me").hide();
     $("#nav-bar").css("padding-top", "0")
     $("#wave-animation").css("padding-top", "0")
     $(".waves").css("height", "40px")
