dc735eabc22c12955913850bf579a5eed6eed686
Rohan Thapliyal <thapliyr@oregonstate.edu>
2024-07-14 13:31:36 -0700

    Starting page with large wave animation, tab links switch css to smaller wave animation

--- main.js
+++ main.js
@@ -8,3 +8,18 @@ $("#header-about").on('click', function () {
     $("#wave-animation").css("padding-top", "0")
     $(".waves").css("height", "40px")
 });
+$("#header-education").on('click', function () {
+    $("#nav-bar").css("padding-top", "0")
+    $("#wave-animation").css("padding-top", "0")
+    $(".waves").css("height", "40px")
+});
+$("#header-experience").on('click', function () {
+    $("#nav-bar").css("padding-top", "0")
+    $("#wave-animation").css("padding-top", "0")
+    $(".waves").css("height", "40px")
+});
+$("#header-projects").on('click', function () {
+    $("#nav-bar").css("padding-top", "0")
+    $("#wave-animation").css("padding-top", "0")
+    $(".waves").css("height", "40px")
+});
