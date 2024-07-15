94b0057b43873f399c24db45486dc1ddbbc93efe
Rohan Thapliyal <thapliyr@oregonstate.edu>
2024-07-14 18:23:40 -0700

    Adding intial version of education page (to be tweaked stylistically)

--- main.js
+++ main.js
@@ -1,29 +1,34 @@
 $("#header-home").on('click', function () {
     $("#about-me").hide();
+    $("#education").hide();
     $("#nav-bar").css("padding-top", "200px")
     $("#wave-animation").css("padding-top", "50px")
     $(".waves").css("height", "100px")
 });
 $("#header-about").on('click', function () {
     $("#about-me").show();
+    $("#education").hide();
     $("#nav-bar").css("padding-top", "0")
     $("#wave-animation").css("padding-top", "0")
     $(".waves").css("height", "40px")
 });
 $("#header-education").on('click', function () {
     $("#about-me").hide();
+    $("#education").show();
     $("#nav-bar").css("padding-top", "0")
     $("#wave-animation").css("padding-top", "0")
     $(".waves").css("height", "40px")
 });
 $("#header-experience").on('click', function () {
     $("#about-me").hide();
+    $("#education").hide();
     $("#nav-bar").css("padding-top", "0")
     $("#wave-animation").css("padding-top", "0")
     $(".waves").css("height", "40px")
 });
 $("#header-projects").on('click', function () {
     $("#about-me").hide();
+    $("#education").hide();
     $("#nav-bar").css("padding-top", "0")
     $("#wave-animation").css("padding-top", "0")
     $(".waves").css("height", "40px")
